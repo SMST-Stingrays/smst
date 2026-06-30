a6425bcc5bcfce7615e307fc8005a33e1d645c88
core <core@coredoes.dev>
2026-06-30 19:00:39 -0400

    feat(home): remove button per client request 2

--- src/lib/components/pageComponents/HomeHeroVidImg.svelte
+++ src/lib/components/pageComponents/HomeHeroVidImg.svelte
@@ -14,20 +14,4 @@
 <div class="relative">
 	<Image src={imgSrc} alt="Hero image" class="w-screen brightness-90" />
 	<h1 class="text-3xl font-bold drop-shadow-[0_0.2px_2.2px_rgba(0,0,0,0.8)] absolute top-4 left-0 right-0 mx-auto text-center">{headerText}</h1>
-<!--	<Dialog.Root>
-		<Dialog.Trigger class={cn(buttonVariants(), "absolute border-yellow-700 border-2 left-0 right-0 mx-auto text-center bottom-2 drop-shadow-[0_0.2px_2.2px_rgba(0,0,0,0.8)]")}>
-			<VideoIcon class="w-4 h-4 mr-2" />
-			{buttonText}
-		</Dialog.Trigger>
-		<Dialog.Content class="w-screen md:min-w-[50vw]">
-			<Dialog.Header>
-				<Dialog.Title>{buttonText}</Dialog.Title>
-			</Dialog.Header>
-			<div class="w-screen md:w-[50vw]">
-				<!-- eslint-disable-next-line -->
-				{@html vidSrc}
-			</div>
-		</Dialog.Content>
-	</Dialog.Root>
--->
 </div>
